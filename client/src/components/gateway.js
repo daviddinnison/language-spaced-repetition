0d2bc7733c4a5b93a5c6882ebc7bb1689c59edd1
daviddinnison <david.g.dinnison@gmail.com>
2018-01-10 14:59:28 -0500

    routing starting to come together...

--- client/src/components/gateway.js
+++ client/src/components/gateway.js
@@ -1,27 +1,23 @@
 import React from 'react';
 import * as Cookies from 'js-cookie';
 import { connect } from 'react-redux';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import QuestionPage from './question-page';
+
 import Dashboard from './dashboard';
 import LoginPage from './login-page';
-import Header from './header';
 import { logUserIn } from '../actions';
 
-export function Gateway(props)  {
+class Gateway extends React.Component {
+    // componentDidMount() {
+    //     const accessToken = Cookies.get('accessToken');
+    //     this.props.dispatch(logUserIn(accessToken));
+    // }
 
-    return (
-        <Router>
-            <div className="app">
-                <main>
-                    <Route exact path="/" component={LoginPage} />
-                    <Route exact path="/dashboard" component={Dashboard} />
-                    <Route exact path="/questions" component={QuestionPage} />
-                    <Route exact path="/header" component={Header} />
-                </main>
-            </div>
-        </Router>
-    )
+    render() {
+        if (!this.props.currentUser) {
+            return <LoginPage />;
+        }
+        return <Dashboard />;
+    }
 }
 
 const mapStateToProps = state => ({
