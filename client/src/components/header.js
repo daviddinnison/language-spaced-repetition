a22536f61b4ac71212e3c59e887f11b7fb2c8b8a
daviddinnison <david.g.dinnison@gmail.com>
2018-01-12 22:29:18 -0500

    header bug fix and underline main link

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -2,10 +2,10 @@ import React from 'react';
 import * as Cookies from 'js-cookie';
 import { connect } from 'react-redux';
 
-
 import './styles/header.css';
 
 import { logUserOut } from '../actions';
+
 
 export class Header extends React.Component {
   logUserOut(event) {
@@ -16,21 +16,16 @@ export class Header extends React.Component {
   }
 
   conditionalLogout() {
-    console.log("LOGGED IN HEADER PROPS", this.props)
-    if (this.props.loggedIn === true) {
-      return (
-        <a href="" onClick={e => this.logUserOut(e)}>Logout</a>
-      );
+    if (this.props.loggedIn) {
+      return <a href="" onClick={e => this.logUserOut(e)} className="logout-button">Logout</a>
     }
   }
 
   render() {
     return (
       <header>
-        <h1>Croatian Buddy</h1>
-        <div class="sub-head">
-          {this.conditionalLogout()}
-        </div>
+        <h1><a href="/" className="home-link">Croatian Buddy</a></h1>
+        {this.conditionalLogout()}
       </header>
     );
   }
